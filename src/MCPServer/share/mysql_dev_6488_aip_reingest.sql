c14aee3a3d9197361c44517f77441be414b9165d
Holly Becker <hbecker@artefactual.com>
2014-07-03 18:17:25 -0700

    Update PREMIS Rights models & forms for mandatory/optional fields
    refs #6488
    
    Update blank & null attributes on PREMIS Rights related tables to reflect
    which fields were mandatory or optional in the PREMIS spec.  If a field is
    optional, but in its own table (eg RightsStatementCopyrightNote), then it
    is marked as blank=null=False, since that row will only be created if that
    field has information.  Update SQL for those tables to reflect the Django
    model.

--- src/MCPServer/share/mysql_dev_6488_aip_reingest.sql
+++ src/MCPServer/share/mysql_dev_6488_aip_reingest.sql
@@ -224,6 +224,32 @@ DELETE FROM MicroServiceChainLinks WHERE pk IN (@d1, @d2, @d3, @d4, @d5, @d6, @d
 -- Update metadata
 ALTER TABLE Dublincore ADD status varchar(8) DEFAULT 'ORIGINAL' COLLATE utf8_unicode_ci NOT NULL;
 ALTER TABLE RightsStatement ADD status varchar(8) DEFAULT 'ORIGINAL' COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE RightsStatementCopyright
+    MODIFY copyrightStatusDeterminationDate longtext NULL,
+    MODIFY copyrightApplicableStartDate longtext NULL,
+    MODIFY copyrightApplicableEndDate longtext NULL
+    ;
+ALTER TABLE RightsStatementCopyrightDocumentationIdentifier MODIFY copyrightDocumentationIdentifierRole longtext NULL;
+ALTER TABLE RightsStatementLicense
+    MODIFY licenseApplicableStartDate longtext NULL,
+    MODIFY licenseApplicableEndDate longtext NULL
+    ;
+ALTER TABLE RightsStatementLicenseDocumentationIdentifier MODIFY licenseDocumentationIdentifierRole longtext NULL;
+ALTER TABLE RightsStatementStatuteInformation
+    MODIFY statuteInformationDeterminationDate longtext NULL,
+    MODIFY statuteApplicableStartDate longtext NULL,
+    MODIFY statuteApplicableEndDate longtext NULL
+    ;
+ALTER TABLE RightsStatementStatuteDocumentationIdentifier MODIFY statuteDocumentationIdentifierRole longtext NULL;
+ALTER TABLE RightsStatementOtherRightsInformation
+    MODIFY otherRightsApplicableStartDate longtext NULL,
+    MODIFY otherRightsApplicableEndDate longtext NULL
+    ;
+ALTER TABLE RightsStatementOtherRightsDocumentationIdentifier MODIFY otherRightsDocumentationIdentifierRole longtext NULL;
+ALTER TABLE RightsStatementRightsGranted
+    MODIFY startDate longtext COLLATE utf8_unicode_ci NULL,
+    MODIFY endDate longtext COLLATE utf8_unicode_ci NULL
+    ;
 
 -- Delete all TasksConfigs that don't have MicroServiceChainLinks pointing at them
 DELETE FROM TasksConfigs USING TasksConfigs LEFT OUTER JOIN MicroServiceChainLinks ON currentTask=TasksConfigs.pk WHERE MicroServiceChainLinks.pk is NULL;
