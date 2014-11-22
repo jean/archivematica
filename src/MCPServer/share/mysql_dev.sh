718adf6cd4cb6dfa80696f2c8671a9f960f2a7c0
Holly Becker <hbecker@artefactual.com>
2014-11-21 17:19:20 -0800

    CONTENTdm: Project client does not query server
    refs #7239
    
    Update MicroServiceChainLinks to not query the CONTENTdm server if the
    upload type is project client.
    
    Conflicts:
    
    	src/MCPServer/share/mysql_dev.sh

--- src/MCPServer/share/mysql_dev.sh
+++ src/MCPServer/share/mysql_dev.sh
@@ -6,6 +6,7 @@ currentDir="`dirname $0`"
 
 # Add post 1.3.1 development to the following files
 echo 'Running mysql_dev1'
+mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7239_contentdm.sql;"
 #mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_XXXX_your_script.sql;"
 
 # ...
