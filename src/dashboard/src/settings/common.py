3af8076a18bf2933d784a0dc031f03b9a4e5532e
Holly Becker <hbecker@artefactual.com>
2015-03-30 11:59:05 -0700

    WIP

--- src/dashboard/src/settings/common.py
+++ src/dashboard/src/settings/common.py
@@ -136,9 +136,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'middleware.common.AJAXSimpleExceptionResponseMiddleware',
     'installer.middleware.ConfigurationCheckMiddleware',
-    'middleware.common.SpecificExceptionErrorPageResponseMiddleware'
+    'middleware.common.SpecificExceptionErrorPageResponseMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
